930308fa7e5ace57866e0dda2c2d5a87e93d4a2f
Michael Milewski <saramic@gmail.com>
2024-03-22 22:52:36 +1100

    Basic blink in arduino

--- experiments/in_location_tracking/src/main.cpp
+++ experiments/in_location_tracking/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_BUILTIN 2 // ESP32 equivalent of Arduino 13
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+long last = 0;
+boolean light = false;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("setup");
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  long now = millis();
+  if (now - last > 1000) // 1 second flash
+  {
+    if (light)
+    {
+      light = false;
+      digitalWrite(LED_BUILTIN, LOW);
+      Serial.print(">led:");
+      Serial.println(0);
+    }
+    else
+    {
+      light = true;
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.print(">led:");
+      Serial.println(1);
+    }
+    last = now;
+  }
 }
